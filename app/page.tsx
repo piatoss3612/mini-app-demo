06528a5dfd50aa7b2f41aa20f1ef9a7bf2ba82ee
rowan <crewe1746@naver.com>
2024-12-25 17:07:59 +0900

    Fix `window is not defined` error

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import WebApp from "@twa-dev/sdk";
+import dynamic from "next/dynamic";
 
-interface UserData {
-  id: number;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  language_code: string;
-  is_premium?: boolean;
-}
+const Main = dynamic(() => import("../components/index"), { ssr: false });
 
 export default function Home() {
-  const [userData, setUserData] = useState<UserData | null>(null);
-
-  useEffect(() => {
-    if (WebApp.initDataUnsafe.user) {
-      setUserData(WebApp.initDataUnsafe.user as UserData);
-    }
-  }, []);
-
-  return (
-    <main className="p-4">
-      {userData ? (
-        <div>
-          <h1 className="text-2xl font-bold">
-            Welcome, {userData.first_name}!
-          </h1>
-          <p>
-            Your user ID is <code>{userData.id}</code>.
-          </p>
-          <p>
-            Your language code is <code>{userData.language_code}</code>.
-          </p>
-          {userData.is_premium && <p>You are a premium user.</p>}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </main>
-  );
+  return <Main />;
 }
